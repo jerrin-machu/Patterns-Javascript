82a7abff87af2d9a5f3c324941d4991a2a569ba3
Jerrin Thomas <jerrinmachu@gmail.com>
2022-09-13 05:30:10 +0530

    hourglass pattern implemented

--- hollow_diamond_pattern.js
+++ hollow_diamond_pattern.js
@@ -14,7 +14,7 @@ for (let i =1; i<=n;i++){
     string = string + "\n"
 }
 
-for( let i = 1; i<=n-1; i++){
+for( let i = 1; i<=n-1; i++){ 
     for( let j = 0; j<i; j++){
         string = string + " "
     }
---
+++ hourglass_pattern.js
@@ -0,0 +1,26 @@
+let n =5
+let string = ""
+for(let i = 0; i < n; i++){
+ for(let j = 0; j<i; j++){
+     string = string + " "
+ }
+ for(let k= (n-i)*2 -1; k>0; k--){
+    string = string + "*"
+ }
+
+ string = string + "\n"
+}
+
+for (let i = 2; i<=n; i++){
+    for (let j = n; j>i; j--){
+        string = string + " "
+    }
+
+    for(let k = 0; k<2*i -1; k++){
+        string = string + "*"
+    }
+
+    string = string + "\n"
+}
+
+console.log(string)
